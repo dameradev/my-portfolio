8404f23023b85e6b4d002fe30f3d3d3978d15570
Damjan Radev <radevdamjan@gmail.com>
2021-01-11 11:13:53 +0100

    Updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,90 @@
 import React from "react";
 import styled from "styled-components";
 
+import LinkedIn from "../assets/images/linkedin-icon.svg";
+import Twitter from "../assets/images/twitter.svg";
+
 const FooterStyles = styled.footer`
-  background: var(--primary);
-  height: 25rem;
-  color: var(--white);
+  padding: 0 5%;
+  height: 35rem;
+
   display: flex;
   align-items: flex-end;
-  justify-content: center;
+  justify-content: space-between;
+  background: var(--primary);
+  color: var(--white);
   clip-path: polygon(0 0, 100% 10vh, 100% 100%, 0 85vh);
+  address {
+    font-style: normal;
+    p:last-of-type {
+      margin-bottom: 0.5rem;
+    }
+    span {
+      margin-right: 1rem;
+    }
+    .email {
+      font-size: 1.4rem;
+    }
+  }
+  a {
+    color: var(--white);
+  }
+
+  .social-links {
+    a {
+      display: flex;
+      align-items: center;
+
+      font-size: 1.4rem;
+      font-style: italic;
+      &:not(:last-of-type) {
+        margin-bottom: 0.5rem;
+      }
+      img {
+        width: 4rem;
+        height: 4rem;
+        &.linkedin {
+          width: 4rem;
+          height: 3rem;
+        }
+        margin-right: 1rem;
+      }
+    }
+  }
 `;
 
 export default function Footer() {
   return (
     <FooterStyles>
-      <p className="center">
-        &copy; Developer Damjan Radev, Designer Nela Nikolovska{" "}
-        {new Date().getFullYear()}
-      </p>
+      <address>
+        <p>
+          <span>Developer</span>
+          <a href="mailto:radevdevelopment@gmail.com">
+            Damjan Radev{" "}
+            <span className="email">radevdevelopment@gmail.com</span>
+          </a>
+        </p>
+        <p>
+          <span>Designer</span>
+          <a href="mailto:nela.nikolovska1@gmail.com">
+            Nela Nikolovska{" "}
+            <span className="email">nela.nikolovska1@gmail.com</span>
+          </a>
+        </p>
+        <p>
+          <span>Phone</span>
+          <a href="tel:+38669652555">+386-69-652-555 (Slovenia)</a>
+        </p>
+      </address>
+
+      <div className="social-links">
+        <a href="https://www.linkedin.com/in/damjan-radev-628120145/">
+          <img src={LinkedIn} className="linkedin" /> LinkedIn
+        </a>
+        <a href="https://www.linkedin.com/in/damjan-radev-628120145/">
+          <img src={Twitter} /> @dameradev
+        </a>
+      </div>
     </FooterStyles>
   );
 }
