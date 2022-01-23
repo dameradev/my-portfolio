57f474f370efefed46fa3fe018016f303ebc3163
Damjan Radev <radevdamjan@gmail.com>
2022-01-23 12:03:31 +0100

    Fixed project page

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,9 +3,11 @@ const { portfolio } = require("./src/utils/data");
 async function turnProjectsIntoPages({ graphql, actions }) {
   // 1. Get a template for this page
   const projectTemplate = path.resolve("./src/pages/project.js");
+  console.log(actions);
 
   // 3. Loop over each pizza and create a page for that pizza
   portfolio.forEach((portfolioItem) => {
+    console.log(portfolioItem);
     const title = portfolioItem.title.toLowerCase().split(" ").join("-");
     actions.createPage({
       // What is the URL for this new page??
@@ -17,8 +19,14 @@ async function turnProjectsIntoPages({ graphql, actions }) {
     });
   });
 }
-async function createPages(params) {
-  // Create pages dynamically
-  // Wait for all promises to be resolved before finishing this function
-  await Promise.all([turnProjectsIntoPages(params)]);
-}
+// async function createPages(params) {
+//   // Create pages dynamically
+//   // Wait for all promises to be resolved before finishing this function
+//   await Promise.all([turnProjectsIntoPages(params)]);
+// }
+
+exports.createPages = async (params) => {
+  // await createBlogPostPages(graphql, actions);
+
+  await turnProjectsIntoPages(params);
+};
