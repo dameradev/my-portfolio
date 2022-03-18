89c5805f52bcec85d32f73ebd921f8ee66b9212e
Damjan Radev <radevdamjan@gmail.com>
2022-03-18 19:41:08 +0100

    Check if window is defined before rendering skyLight component

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,8 +16,6 @@ const HomePage = ({ data }) => {
   const backendData = recivedData.about.backend;
   const portfolioData = recivedData.portfolio;
 
-  console.log(data);
-
   const firstTestimonial = useRef();
   const secondTestimonial = useRef();
   const fifthTestimonial = useRef();
@@ -82,9 +80,11 @@ const HomePage = ({ data }) => {
               best route to complete a task. He is very smart, reliable and
               always available. He will always remain in our hearts. Thank you
               Damjan for everything!
-              <SkyLight hideOnOverlayClicked ref={firstTestimonial}>
-                <Image src="upwork-testamonial.png" />
-              </SkyLight>
+              {typeof window !== `undefined` && (
+                <SkyLight hideOnOverlayClicked ref={firstTestimonial}>
+                  <Image src="upwork-testamonial.png" />
+                </SkyLight>
+              )}
               <a
                 className="clickable"
                 onClick={() => firstTestimonial.current.show()}
@@ -107,9 +107,11 @@ const HomePage = ({ data }) => {
             <p className="testamonial__content">
               Damjan was great! Great work and a quick turnaround. Will
               absolutely work with him again in the future!
-              <SkyLight hideOnOverlayClicked ref={secondTestimonial}>
-                <Image src="testamonial-try-frame.png" />
-              </SkyLight>
+              {typeof window !== `undefined` && (
+                <SkyLight hideOnOverlayClicked ref={secondTestimonial}>
+                  <Image src="testamonial-try-frame.png" />
+                </SkyLight>
+              )}
               <a
                 className="clickable"
                 onClick={() => secondTestimonial.current.show()}
@@ -134,9 +136,11 @@ const HomePage = ({ data }) => {
               together for almost a year and he constantly delivers the project
               and features. He is able to produce quality, and tested coded and
               work independently. I would recommend anyone working with him
-              <SkyLight hideOnOverlayClicked ref={fifthTestimonial}>
-                <Image src="testamonial-ndw.png" />
-              </SkyLight>
+              {typeof window !== `undefined` && (
+                <SkyLight hideOnOverlayClicked ref={fifthTestimonial}>
+                  <Image src="testamonial-ndw.png" />
+                </SkyLight>
+              )}
               <a
                 className="clickable"
                 onClick={() => fifthTestimonial.current.show()}
@@ -158,42 +162,52 @@ const HomePage = ({ data }) => {
         <h1>Certificates</h1>
         <ul>
           <li>
-            <SkyLight hideOnOverlayClicked ref={firstCert}>
-              <Image src="css.png" />
-            </SkyLight>
+            {typeof window !== `undefined` && (
+              <SkyLight hideOnOverlayClicked ref={firstCert}>
+                <Image src="css.png" />
+              </SkyLight>
+            )}
             <p onClick={() => firstCert.current.show()}>
               Css-The Complete Guide(Flexbox, grid&Sass)
             </p>
           </li>
           <li>
-            <SkyLight hideOnOverlayClicked ref={secondCert}>
-              <Image src="node-js.png" />
-            </SkyLight>
+            {typeof window !== `undefined` && (
+              <SkyLight hideOnOverlayClicked ref={secondCert}>
+                <Image src="node-js.png" />
+              </SkyLight>
+            )}
             <p onClick={() => secondCert.current.show()}>
               NodeJs-The Complete Guide(incl. MVC, REST APIs, GraphQL)
             </p>
           </li>
           <li>
-            <SkyLight hideOnOverlayClicked ref={thirdCert}>
-              <Image src="master-gatsby.png" />
-            </SkyLight>
+            {typeof window !== `undefined` && (
+              <SkyLight hideOnOverlayClicked ref={thirdCert}>
+                <Image src="master-gatsby.png" />
+              </SkyLight>
+            )}
 
             <p onClick={() => thirdCert.current.show()}>
               Master Gatsby Advanced Course
             </p>
           </li>
           <li>
-            <SkyLight hideOnOverlayClicked ref={forthCert}>
-              <Image src="advanced-react.png" />
-            </SkyLight>
+            {typeof window !== `undefined` && (
+              <SkyLight hideOnOverlayClicked ref={forthCert}>
+                <Image src="advanced-react.png" />
+              </SkyLight>
+            )}
             <p onClick={() => forthCert.current.show()}>
               Advanced React and GraphQL and Prisma
             </p>
           </li>
           <li>
-            <SkyLight hideOnOverlayClicked ref={fifthCert}>
-              <Image src="react.png" />
-            </SkyLight>
+            {typeof window !== `undefined` && (
+              <SkyLight hideOnOverlayClicked ref={fifthCert}>
+                <Image src="react.png" />
+              </SkyLight>
+            )}
             <p onClick={() => fifthCert.current.show()}>
               Modern React and Redux
             </p>
